bf082c8bbb73263576e8acc2e0f16dd9634e08fd
abba <abba@workdeb>
2014-11-21 16:54:12 +0600

    fixed

---
+++ Model/SearchModel.php
@@ -0,0 +1,37 @@
+<?php
+namespace Xtlan\Core\Model;
+
+use yii\base\Model;
+use yii\db\QueryInterface;
+
+/**
+ * SearchModel
+ *
+ * @version 1.0.0
+ * @copyright Copyright 2011 by Kirya <cloudkserg11@gmail.com>
+ * @author Kirya <cloudkserg11@gmail.com>
+ */
+class SearchModel extends Model
+{
+
+    /**
+     * search
+     *
+     * @param QueryInterface $query
+     * @return void
+     */
+    public function search(QueryInterface $query)
+    {
+        foreach ($this->attributes as $name => $attribute) {
+            $methodName = 'for' . ucfirst($name);
+            if (method_exists($query, $methodName)) {
+                $query->$methodName($attribute);
+            }
+        }
+
+
+
+        return $query;
+    }
+
+}

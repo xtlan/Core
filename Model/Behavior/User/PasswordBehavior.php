37feffe0a6597b1483d0bf5400709636094a446e
abba <abba@workdeb>
2014-11-28 11:07:42 +0600

    yii

---
+++ Model/Behavior/User/PasswordBehavior.php
@@ -0,0 +1,105 @@
+<?php
+namespace Xtlan\Core\Model\Behavior\User;
+
+use yii\base\Behavior;
+use yii\db\ActiveRecord;
+use Yii;
+
+/**
+ * PasswordBehavior
+ *
+ * @version 1.0.0
+ * @copyright Copyright 2011 by Kirya <cloudkserg11@gmail.com>
+ * @author Kirya <cloudkserg11@gmail.com>
+ */
+class PasswordBehavior extends Behavior
+{
+
+    /**
+     * authField
+     *
+     * @var string|null
+     */
+    public $authField; 
+
+    /**
+     * passwordField
+     *
+     * @var string
+     */
+    public $passwordField = 'password';
+
+
+    /**
+     * events
+     *
+     * @return array
+     */
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_BEFORE_INSERT => 'beforeInsert',
+            ActiveRecord::EVENT_BEFORE_UPDATE => 'beforeUpdate'
+        ];
+    }
+
+    /**
+     * beforeInsert
+     *
+     * @param mixed $event
+     * @return void
+     */
+    public function beforeInsert($event)
+    {
+        $this->updatePassword($event);
+
+        if (isset($this->authField)) {
+            $this->createAuthKey($event);
+        }
+    }
+
+    /**
+     * beforeUpdate
+     *
+     * @param mixed $event
+     * @return void
+     */
+    public function beforeUpdate($event)
+    {
+        $this->updatePassword($event);
+    }
+
+    /*
+     ** Функция перед сохранением 
+     * пользователя
+     */
+    public function updatePassword($event)
+    {
+        $sender = $event->sender;
+
+        if (in_array($this->passwordField, $sender->dirtyAttributes)) {
+            $password = $sender->getAttribute($this->passwordField);
+            $hash = Yii::$app->security->generatePasswordHash($password);
+
+            $sender->setAttribute($this->passwordField, $hash);
+        }
+
+    }
+
+    /**
+     * createAuthKey
+     *
+     * @param mixed $event
+     * @return void
+     */
+    private function createAuthKey($event)
+    {
+        $sender = $event->sender;
+        
+        $sender->setAttribute($this->authField, Yii::$app->security->generateRandomString());
+    }
+    
+
+}
+
+
